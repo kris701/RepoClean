81651c9848b87f5b1929032c8e3491817bad5ebf
Kristian Skov <kris701kj@gmail.com>
2025-12-04 11:18:51 +0100

    Fixed a bug where the system wouldnt delete folders with the "R" attribute

--- RepoClean/Program.cs
+++ RepoClean/Program.cs
@@ -66,9 +66,11 @@ namespace RepoClean
 
         private static void DeleteDirectory(string dir)
         {
+            var dirInfo = new DirectoryInfo(dir);
             try
             {
-                Directory.Delete(dir, true);
+				SetAttributesNormal(dirInfo);
+				Directory.Delete(dir, true);
                 WriteLineColor("Removed", ConsoleColor.Green);
             }
             catch (Exception e)
@@ -77,7 +79,18 @@ namespace RepoClean
             }
         }
 
-        public static bool Confirm()
+		private static void SetAttributesNormal(DirectoryInfo dir)
+		{
+			foreach (var subDir in dir.GetDirectories())
+				SetAttributesNormal(subDir);
+			foreach (var file in dir.GetFiles())
+			{
+				file.Attributes = FileAttributes.Normal;
+			}
+			dir.Attributes = FileAttributes.Normal;
+		}
+
+		public static bool Confirm()
         {
             ConsoleKey response;
             do
